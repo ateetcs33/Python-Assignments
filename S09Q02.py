01269578e855d10a37c9c5bd452578268b1b4d15
Atitkumar Shingatalur <ateetcs33@gmail.com>
2020-06-21 10:53:55 +0530

    parameterized the function

--- S09Q02.py
+++ S09Q02.py
@@ -1,10 +1,8 @@
 #/usr/local/bin/python3
 import S08Q01 as base
 
-def string_search_and_replace():
-    main_str = base.get_user_input("Enter the main String :")
-    search_str = base.get_user_input("Enter the String to be searched :")
-    replace_str = base.get_user_input("Enter the new String to replace :")      
+def string_search_and_replace(main_str, search_str, replace_str):
+       
     if main_str.find(search_str) != -1:
         main_str = main_str.replace(search_str, replace_str)
         print("String after replacing \"" + search_str + "\" with \"" + replace_str +"\" is :\n", main_str)
@@ -14,5 +12,8 @@ def string_search_and_replace():
     
 def main():
     if __name__ == "__main__":
-        string_search_and_replace()
+        main_str = base.get_user_input("Enter the main String :")
+        search_str = base.get_user_input("Enter the String to be searched :")
+        replace_str = base.get_user_input("Enter the new String to replace :") 
+        string_search_and_replace(main_str, search_str, replace_str)
 main()
